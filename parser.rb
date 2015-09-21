5757cedd1835ff905930f08def4838cbd9f3b810
Michael Ramuta <michaelramuta@gmail.com>
2015-09-21 14:09:13 -0500

    can sort by last name

--- parser.rb
+++ parser.rb
@@ -1,15 +1,18 @@
 require 'csv'
 
 class Controller
-  attr_accessor :data, :people
+  attr_accessor :people
   def initialize(file)
-    @data = CSV.read(file, headers: true)
-    @people = @data.map{ |person| person.to_hash}
+    data = CSV.read(file, headers: true)
+    @people = data.map{ |person| person.to_hash}
   end
 
   def filter_by_email(people)
     people.uniq{|person| person["email"]}
   end
 
+  def sort_by_last_name(people)
+    people.sort{|person1, person2| person1["last_name"] <=> person2["last_name"]}
+  end
 
 end
--- parser_spec.rb
+++ parser_spec.rb
@@ -3,7 +3,7 @@ require_relative 'parser'
 describe "Parser" do
   let(:controller) { Controller.new('sample_data.csv') }
   it 'correctly reads the data' do
-    expect(controller.data.count).to be(3)
+    expect(controller.people.count).to be(3)
   end
 
   it 'can convert the data to an array of hashes' do
@@ -12,5 +12,9 @@ describe "Parser" do
 
   it 'can filter uniquely by email' do
     expect(controller.filter_by_email(controller.people).count).to be(2)
+  end
+
+  it 'can sort by last name' do
+    expect(controller.sort_by_last_name(controller.people)[0]['last_name']).to eq('Appleseed')
   end
 end
