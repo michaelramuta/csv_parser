e8916857bd581e62db7955d6449072ec6ddfd613
Michael Ramuta <michaelramuta@gmail.com>
2015-09-21 15:48:45 -0500

    add view

--- parser.rb
+++ parser.rb
@@ -21,6 +21,25 @@ class Controller
   def sort_by_last_name(people)
     people.sort{|person1, person2| person1["last_name"] <=> person2["last_name"]}
   end
+
+  def print_list(people)
+    people.each do |person|
+      View.print_person(person)
+    end
+  end
+
+  def run
+    read_file(filename)
+    convert_file_to_hash(data)
+    sorted_people = sort_by_last_name(filter_by_email(people))
+    print_list(sorted_people)
+  end
+end
+
+class View
+  def self.print_person(person)
+    puts "#{person['email']},#{person['first_name']},#{person['last_name']}"
+  end
 end
 
 controller = Controller.new('sample_data.csv')
