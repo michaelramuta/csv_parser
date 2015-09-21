6c11a14a85703fc18c0d275fdec35d491b302258
Michael Ramuta <michaelramuta@gmail.com>
2015-09-21 13:53:14 -0500

    convert data to array of hashes

--- parser.rb
+++ parser.rb
@@ -1,9 +1,10 @@
 require 'csv'
 
 class Controller
-  attr_accessor :data
+  attr_accessor :data, :people
   def initialize(file)
     @data = CSV.read(file, headers: true)
+    @people = @data.map{ |person| person.to_hash}
   end
 
 end
--- parser_spec.rb
+++ parser_spec.rb
@@ -5,4 +5,8 @@ describe "Parser" do
   it 'correctly reads the data' do
     expect(controller.data.count).to be(3)
   end
+
+  it 'can convert the data to an array of hashes' do
+    expect(controller.people[0]['email']).to eq('steve.johnson@gmail.com')
+  end
 end
